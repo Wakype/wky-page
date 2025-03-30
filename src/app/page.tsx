72c1de62098a54c993981231feedb6ac97c99a6c
Wakype <hilmim152bisnis@gmail.com>
2025-03-30 22:14:41 +0700

    add: Bento box Layout, Social Media Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,138 @@
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
+import Link from "next/link";
+import Ikura from "../../public/ikura.jpg";
+import {
+  FaDiscord,
+  FaGithub,
+  FaHandHoldingHeart,
+  FaInstagram,
+  FaLinkedin,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const SOCIAL_MEDIA = [
+    {
+      name: "Github",
+      url: "https://github.com/Wakype",
+      icon: <FaGithub className="group-hover:text-gray-500" />,
+    },
+    {
+      name: "Instagram",
+      url: "https://instagram.com/wakype",
+      icon: <FaInstagram className="group-hover:text-pink-500" />,
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/muhammad-hilmi-487bb2235/",
+      icon: <FaLinkedin className="group-hover:text-blue-500" />,
+    },
+    {
+      name: "Saweria",
+      url: "https://saweria.co",
+      icon: <FaHandHoldingHeart className="group-hover:text-orange-500" />,
+    },
+    {
+      name: "Youtube",
+      url: "https://youtube.com",
+      icon: <FaYoutube className="group-hover:text-red-500" />,
+    },
+    {
+      name: "Discord",
+      url: "https://discord.com",
+      icon: <FaDiscord className="group-hover:text-indigo-500" />,
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com",
+      icon: <FaXTwitter className="group-hover:text-sky-500" />,
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="relative bg-[#030712] scroll">
+      <Image
+        src={Ikura}
+        alt="Picture of the author"
+        className="top-0 left-0 fixed opacity-50"
+      />
+
+      <main className="min-h-screen grid grid-cols-8 grid-rows-5 gap-4 p-5 text-center">
+        <Card className="row-span-2 col-span-2">
+          <CardContent>Profil</CardContent>
+        </Card>
+        <Card className="col-span-3 row-span-2">
+          <CardContent>About</CardContent>
+        </Card>
+        <Card className="col-span-2 row-span-3">
+          <CardContent>Skills</CardContent>
+        </Card>
+        <Card className="row-span-full">
+          <CardContent>Adjust</CardContent>
+        </Card>
+
+        <Card className="col-span-5">
+          <CardContent>Projects</CardContent>
+        </Card>
+
+        <Card className="row-span-2 col-span-2">
+          <CardContent>DM</CardContent>
+        </Card>
+        <Card className="col-span-3">
+          <CardContent>Certificate</CardContent>
+        </Card>
+        <Card className="col-span-2 row-span-2">
+          <CardContent>Blog</CardContent>
+        </Card>
+
+        <Card className="col-span-3">
+          <CardContent>Time</CardContent>
+        </Card>
+        <Card className="cursor-pointer group">
+          <CardContent className="flex items-center justify-center gap-x-1">
+            <p className="group-hover:animate-spin group-hover:text-xl transition-all ease-in-out font-bold -rotate-45">?</p>
+            <p className="group-hover:animate-spin group-hover:text-xl transition-all ease-in-out font-bold text-lg">?</p>
+            <p className="group-hover:animate-spin group-hover:text-xl transition-all ease-in-out font-bold rotate-45">?</p>
+          </CardContent>
+        </Card>
+        <div className="col-span-7 flex items-center gap-x-4">
+          {SOCIAL_MEDIA.map((social) => {
+            return (
+              <Link
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={social.name}
+                className="w-full"
+              >
+                <Card className="group cursor-pointer">
+                  <CardContent>
+                    <div className="flex items-center gap-x-2 justify-center">
+                      {social.icon}
+                      <p>{social.name}</p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Link>
+            );
+          })}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="text-center mt-72 pb-5 relative z-20 flex justify-center ">
+        <Card className="w-fit px-5 py-3 hover:translate-y-0">
+          <div className="">
+            <p className="text-gray-300 text-sm">
+              Made with 💖 and <Badge variant={"secondary"}>(＾▽＾)</Badge>
+            </p>
+            <p className="text-gray-300 text-sm">
+              © 2025 Muhammad Hilmi. All rights reserved.
+            </p>
+          </div>
+        </Card>
       </footer>
     </div>
   );
